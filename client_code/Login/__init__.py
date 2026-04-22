a84dcc4bc650d6fd06e794ff177ef84d39e324d7
darsh.sheth@education.nsw.gov.au <darsh.sheth@education.nsw.gov.au>
2026-04-22 12:04:16 +0000

    Created Signin, Login and Dashboard; Edited settings; Deleted Form1

--- client_code/Login/__init__.py
+++ client_code/Login/__init__.py
@@ -1,13 +1,19 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import LoginTemplate
 from anvil import *
 import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class Form1(Form1Template):
+
+class Login(LoginTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run when the form opens.
+
+  @handle("", "hide")
+  def form_hide(self, **event_args):
+    """This method is called when the form is removed from the page"""
+    pass  # Write code here
---
+++ client_code/Signin/__init__.py
@@ -0,0 +1,39 @@
+from ._anvil_designer import SigninTemplate
+from anvil import *
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+class Signin(SigninTemplate):
+  def __init__(self, **properties):
+    self.init_components(**properties)
+    self.error_label.visible = False
+
+  @handle("sign_in_button", "click")
+  def sign_in_button_click(self, **event_args):
+    email = self.email_enter.text.strip()
+    username = self.username_enter.text.strip()
+    password = self.password_enter.text.strip()
+
+    if not email.endswith("@gmail.com") and not email.endswith("@education.nsw.gov.au"):
+      self.error_label.visible = True
+      return
+
+    self.error_label.visible = False
+
+    if not username or not password:
+      alert("Please fill in all fields.")
+      return
+
+    open_form('Dashboard')
+
+    
+  @handle("login_page_button", "click")
+  def login_page_button_click(self, **event_args):
+    open_form('Login')
+
+  @handle("", "hide")
+  def form_hide(self, **event_args):
+    """This method is called when the form is removed from the page"""
+    pass
