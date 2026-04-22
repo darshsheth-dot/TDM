09adb018bfbe7cb17f4f5f619d9703b6a7a0bfc2
darsh.sheth@education.nsw.gov.au <darsh.sheth@education.nsw.gov.au>
2026-04-22 12:26:21 +0000

    Edited Signin, settings and Login

--- client_code/Login/__init__.py
+++ client_code/Login/__init__.py
@@ -5,15 +5,33 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-
 class Login(LoginTemplate):
   def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.error_label2.visible = False
 
-    # Any code you write here will run when the form opens.
+  @handle("login_button", "click")
+  def login_button_click(self, **event_args):
+    email = self.email_enter2.text.strip()
+    password = self.password_enter2.text.strip()
+
+    if not email.endswith("@gmail.com") and not email.endswith("@education.nsw.gov.au"):
+      self.error_label2.visible = True
+      return
+
+    self.error_label2.visible = False
+
+    if not password:
+      alert("Please fill in all fields.")
+      return
+
+    open_form('Dashboard')
+
+  @handle("sign_in_page_button", "click")
+  def sign_in_page_button_click(self, **event_args):
+    open_form('Signin')
 
   @handle("", "hide")
   def form_hide(self, **event_args):
     """This method is called when the form is removed from the page"""
-    pass  # Write code here
+    pass
