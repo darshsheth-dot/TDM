12d03afb2abe5548dfbfde1c798ec5e223fe395a
ishaan.sarin@education.nsw.gov.au <ishaan.sarin@education.nsw.gov.au>
2026-04-29 07:35:19 +0000

    Edited Blok, Dashboard, MindMap, Routine and 1 more

--- client_code/MindMap/__init__.py
+++ client_code/MindMap/__init__.py
@@ -12,3 +12,23 @@ class MindMap(MindMapTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run when the form opens.
+
+  @handle("Dashboard_button", "click")
+  def Dashboard_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('Dashboard')
+
+  @handle("mindmap_button", "click")
+  def mindmap_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('MindMap')
+
+  @handle("routine_button", "click")
+  def routine_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('Routine')
+
+  @handle("blok_button", "click")
+  def blok_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('Blok')
