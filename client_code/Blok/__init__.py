7856a4fa0b693a64116a92903e0d620cf3005ba0
ishaan.sarin@education.nsw.gov.au <ishaan.sarin@education.nsw.gov.au>
2026-05-01 09:15:58 +0000

    Created Routine and Blok; Edited settings and Dashboard

---
+++ client_code/Blok/__init__.py
@@ -0,0 +1,49 @@
+from ._anvil_designer import BlokTemplate
+from anvil import *
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+
+class Blok(BlokTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run when the form opens.
+
+  @handle("Dashboard_button", "click")
+  def Dashboard_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form("Dashboard")
+
+  @handle("mindmap_button", "click")
+  def mindmap_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form("MindMap")
+
+  @handle("Math", "click")
+  def Math_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form("Math")
+
+  @handle("Science", "click")
+  def Science_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form("Science")
+
+  @handle("English", "click")
+  def English_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form("English")
+
+  @handle("feedback_form_button", "click")
+  def feedback_form_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form("Fback_Form")
+
+  @handle("", "refreshing_data_bindings")
+  def form_refreshing_data_bindings(self, **event_args):
+    """This method is called when refresh_data_bindings is called"""
+    pass  # Write Code Here
--- client_code/Dashboard/__init__.py
+++ client_code/Dashboard/__init__.py
@@ -5,48 +5,42 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-
 class Dashboard(DashboardTemplate):
   def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-    # Any code you write here will run when the form opens.
-
-  @handle("Dashboard_button", "click")
-  def Dashboard_button_click(self, **event_args):
-    """This method is called when the button is clicked"""
+  @handle("dashboard_button", "click")
+  def dashboard_button_click(self, **event_args):
     open_form('Dashboard')
 
   @handle("mindmap_button", "click")
   def mindmap_button_click(self, **event_args):
-    """This method is called when the button is clicked"""
     open_form('MindMap')
 
+  @handle("routine_button", "click")
+  def routine_button_click(self, **event_args):
+    open_form('Routine')
 
+  @handle("blok_button", "click")
+  def blok_button_click(self, **event_args):
+    open_form('Blok')
 
   @handle("Math", "click")
   def Math_click(self, **event_args):
-    """This method is called when the button is clicked"""
     open_form('Math')
 
   @handle("Science", "click")
   def Science_click(self, **event_args):
-    """This method is called when the button is clicked"""
     open_form('Science')
 
   @handle("English", "click")
   def English_click(self, **event_args):
-    """This method is called when the button is clicked"""
     open_form('English')
 
   @handle("feedback_form_button", "click")
   def feedback_form_button_click(self, **event_args):
-    """This method is called when the button is clicked"""
     open_form('Fback_Form')
 
   @handle("", "refreshing_data_bindings")
   def form_refreshing_data_bindings(self, **event_args):
-    """This method is called when refresh_data_bindings is called"""
-    pass  # Write Code Here
-
+    pass
