605252bc6953af3228a93a5e571cf14dc7c77e46
darsh.sheth@education.nsw.gov.au <darsh.sheth@education.nsw.gov.au>
2026-05-02 04:06:55 +0000

    Created FocusMode; Edited Blok

--- client_code/Blok/__init__.py
+++ client_code/Blok/__init__.py
@@ -5,29 +5,39 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-
 class Blok(BlokTemplate):
   def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.timer_dropdown.items = ["25 minutes", "30 minutes", "45 minutes", "60 minutes"]
+    self.timer_dropdown.selected_value = "25 minutes"
 
-    # Any code you write here will run when the form opens.
   @handle("Dashboard_button", "click")
   def Dashboard_button_click(self, **event_args):
-    """This method is called when the button is clicked"""
     open_form('Dashboard')
 
   @handle("mindmap_button", "click")
   def mindmap_button_click(self, **event_args):
-    """This method is called when the button is clicked"""
     open_form('MindMap')
 
   @handle("routine_button", "click")
   def routine_button_click(self, **event_args):
-    """This method is called when the button is clicked"""
     open_form('Routine')
 
   @handle("blok_button", "click")
   def blok_button_click(self, **event_args):
-    """This method is called when the button is clicked"""
     open_form('Blok')
+
+  @handle("play_button", "click")
+  def play_button_click(self, **event_args):
+    selected_time = self.timer_dropdown.selected_value
+    minutes = int(selected_time.split(" ")[0])
+
+    blocked_apps = []
+    if self.tiktok_check.checked:
+      blocked_apps.append("TikTok")
+    if self.snapchat_check.checked:
+      blocked_apps.append("Snapchat")
+    if self.instagram_check.checked:
+      blocked_apps.append("Instagram")
+    if self.youtube_check.checked:
+      blocked_app
