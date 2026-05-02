0ae738cc5e0166b5b8bfb43f87af6c63031416a5
darsh.sheth@education.nsw.gov.au <darsh.sheth@education.nsw.gov.au>
2026-05-02 08:23:06 +0000

    Created GeneratedRoutine; Edited settings, Dashboard, Routine and Routine.RowTemplate1

--- client_code/Routine/__init__.py
+++ client_code/Routine/__init__.py
@@ -5,29 +5,65 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-
 class Routine(RoutineTemplate):
   def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.load_tasks()
 
-    # Any code you write here will run when the form opens.
+  def load_tasks(self):
+    self.routine_grid.items = app_tables.tasks.search()
+
+  @handle("add_row_button", "click")
+  def add_row_button_click(self, **event_args):
+    app_tables.tasks.add_row(
+      task_name="",
+      subject="",
+      due_date="",
+      priority="",
+      username=""
+    )
+    self.load_tasks()
+
+  @handle("remove_row_button", "click")
+  def remove_row_button_click(self, **event_args):
+    selected = self.routine_grid.get_selected_row()
+    if selected:
+      selected.delete()
+      self.load_tasks()
+    else:
+      alert("Please select a row to remove first!")
+
+  @handle("generate_button", "click")
+  def generate_button_click(self, **event_args):
+    tasks = list(app_tables.tasks.search())
+    if not tasks:
+      alert("No tasks to generate a routine from!")
+      return
+
+    priority_order = {"High": 1, "Medium": 2, "Low": 3}
+    sorted_tasks = sorted(tasks, key=lambda t: (
+      priority_order.get(t["priority"], 4),
+      t["due_date"] or ""
+    ))
+
+    open_form('GeneratedRoutine', tasks=sorted_tasks)
+
   @handle("Dashboard_button", "click")
   def Dashboard_button_click(self, **event_args):
-    """This method is called when the button is clicked"""
     open_form('Dashboard')
 
   @handle("mindmap_button", "click")
   def mindmap_button_click(self, **event_args):
-    """This method is called when the button is clicked"""
     open_form('MindMap')
 
   @handle("routine_button", "click")
   def routine_button_click(self, **event_args):
-    """This method is called when the button is clicked"""
     open_form('Routine')
 
   @handle("blok_button", "click")
   def blok_button_click(self, **event_args):
-    """This method is called when the button is clicked"""
     open_form('Blok')
+
+  @handle("", "hide")
+  def form_hide(self, **event_args):
+    pass
